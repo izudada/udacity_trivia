f5e31b2b3956c4523ec536c4e1a2ee9190d88d88
Anthony <tonyudeagbala@gmail.com>
2022-06-18 17:42:31 +0100

    Add error handlers

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -105,5 +105,38 @@ def create_app(test_config=None):
     including 404 and 422.
     """
 
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False, 
+            "error": 404,
+            "message": "Not found"
+            }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False, 
+            "error": 422,
+            "message": "Unprocessable"
+            }), 422
+
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False, 
+            "error": 400,
+            "message": "Bad request"
+            }), 400
+    
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return jsonify({
+            "success": False, 
+            "error": 405,
+            "message": "Method not allowed"
+            }), 405
+
     return app
 
